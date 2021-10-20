b2f15f68586973bd0ef5c03f31bf03f2a08bedba
David Morrow <dperrymorrow@gmail.com>
2021-10-20 12:19:04 -0700

    setting up workshops

--- scripts/build.js
+++ scripts/build.js
@@ -5,7 +5,8 @@ const fs = require("fs");
 const instagram = require("../_src/data/instagram.json");
 const grid = require("../_src/data/grid.json");
 const bios = require("../_src/data/bios.json");
-const experiences = require("../_src/data/experiences.json");
+const workshops = require("../_src/data/workshops.json");
+const meta = require("./meta.json");
 
 const source = path.join(__dirname, "../_src/");
 const dest = path.join(__dirname, "../docs/");
@@ -14,15 +15,55 @@ const files = ["index.ejs", "workshops.ejs", "about.ejs"];
 const stylusFile = path.join(source, "styles.stylus");
 const cssFile = path.join(dest, "styles.css");
 
+meta.version += 1;
+
 files.forEach((fileName) => {
-  ejs.renderFile(path.join(source, fileName), {
-    instagram, grid, bios, experiences,
-  }, {}, (err, str) => {
-    fs.writeFileSync(path.join(dest, fileName.replace(".ejs", ".html")), str);
-  });
+  ejs.renderFile(
+    path.join(source, fileName),
+    {
+      instagram,
+      grid,
+      bios,
+      workshops,
+      meta,
+    },
+    {},
+    (err, str) => {
+      if (err) {
+        throw err;
+      }
+      fs.writeFileSync(path.join(dest, fileName.replace(".ejs", ".html")), str);
+    },
+  );
 });
 
-stylus.render(fs.readFileSync(stylusFile, "utf-8"), { filename: cssFile }, (err, css) => {
-  if (err) throw err;
-  fs.writeFileSync(path.join(dest, "styles.css"), css);
+workshops.forEach((workshop) => {
+  ejs.renderFile(
+    path.join(source, "workshop-details.ejs"),
+    {
+      workshop,
+      meta,
+    },
+    {},
+    (err, str) => {
+      if (err) {
+        throw err;
+      }
+      fs.writeFileSync(path.join(dest, `${workshop.slug}.html`), str);
+    },
+  );
 });
+
+stylus.render(
+  fs.readFileSync(stylusFile, "utf-8"),
+  { filename: cssFile },
+  (err, css) => {
+    if (err) throw err;
+    fs.writeFileSync(path.join(dest, "styles.css"), css);
+  },
+);
+
+fs.writeFileSync(
+  path.join(__dirname, "meta.json"),
+  JSON.stringify(meta, null, 2),
+);
